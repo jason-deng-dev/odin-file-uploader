0810d1fc39e081afc0d0b0554809b6e5fb8b7129
Jason <jason13.deng@gmail.com>
2026-03-14 16:19:37 +0900

    feat: add default render to indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,23 +1,32 @@
-import { prisma } from '../lib/prisma.js';
-import { body, validationResult } from 'express-validator';
-import { getAllFiles } from '../controllers/fileController.js';
-import { getAllFolders } from '../controllers/folderController.js';
+import { prisma } from "../lib/prisma.js";
+import { body, validationResult } from "express-validator";
+import { getAllFiles } from "../controllers/fileController.js";
+import { getAllFolders } from "../controllers/folderController.js";
+
+// handle default behavior
+const renderIndex = async (req, res, extras = {}) => {
+    if (req.user) {
+        const folders = await getAllFolders(req.user.id);
+        res.render("index", { folders, styles: "index.css", findFileId: null, fileInfoId: null, ...extras });
+    } else {
+        res.render("index", { styles: "index.css", findFileId: null, fileInfoId: null, ...extras });
+    }
+};
 
 export const indexGet = async (req, res, next) => {
-    if (req.user) {
-        const folders = await getAllFolders(req.user.id)
-        res.render('index', {folders, styles: 'index.css', fileId: null})
-    }　else {
-        res.render('index', {styles: 'index.css', fileId: null})
-    }
-}
+    try {
+        await renderIndex(req, res);
+    } catch (err) { next(err); }
+};
 
 export const indexEditFileGet = async (req, res, next) => {
-    const fileId = req.params.file_id;
-    if (req.user) {
-        const folders = await getAllFolders(req.user.id)
-        res.render('index', {folders, styles: 'index.css', fileId})
-    }　else {
-        res.render('index', {styles: 'index.css', fileId: null})
-    }
-}
+    try {
+        await renderIndex(req, res, { findFileId: req.params.file_id });
+    } catch (err) { next(err); }
+};
+
+export const indexFileInfoGet = async (req, res, next) => {
+    try {
+        await renderIndex(req, res, { fileInfoId: req.params.file_id });
+    } catch (err) { next(err); }
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as indexController from '../controllers/indexController.js';
-import { indexGet, indexEditFileGet } from '../controllers/indexController.js';
+import { indexGet, indexEditFileGet, indexFileInfoGet } from '../controllers/indexController.js';
 
 const indexRouter = Router();
 
 indexRouter.get('/', indexGet)
 
 indexRouter.get('/editFile/:file_id', indexEditFileGet)
+indexRouter.get('/fileInfo/:file_id', indexFileInfoGet)
 
 export default indexRouter;
