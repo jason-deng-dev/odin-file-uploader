1d6d9f099e6d1cdd4c18a4582d7ecb604962622c
Jason <jason13.deng@gmail.com>
2026-03-14 16:42:01 +0900

    feat: toggle file edit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,28 +5,56 @@ import { getAllFolders } from "../controllers/folderController.js";
 
 // handle default behavior
 const renderIndex = async (req, res, extras = {}) => {
-    if (req.user) {
-        const folders = await getAllFolders(req.user.id);
-        res.render("index", { folders, styles: "index.css", findFileId: null, fileInfoId: null, ...extras });
-    } else {
-        res.render("index", { styles: "index.css", findFileId: null, fileInfoId: null, ...extras });
-    }
+	if (req.user) {
+		const folders = await getAllFolders(req.user.id);
+		res.render("index", {
+			folders,
+			styles: "index.css",
+			findFileId: null,
+			fileInfoId: null,
+			editFolderId: null,
+			...extras,
+		});
+	} else {
+		res.render("index", {
+			styles: "index.css",
+			findFileId: null,
+			fileInfoId: null,
+			editFolderId: null,
+			...extras,
+		});
+	}
 };
 
 export const indexGet = async (req, res, next) => {
-    try {
-        await renderIndex(req, res);
-    } catch (err) { next(err); }
+	try {
+		await renderIndex(req, res);
+	} catch (err) {
+		next(err);
+	}
 };
 
 export const indexEditFileGet = async (req, res, next) => {
-    try {
-        await renderIndex(req, res, { findFileId: req.params.file_id });
-    } catch (err) { next(err); }
+	try {
+		await renderIndex(req, res, { findFileId: req.params.file_id });
+	} catch (err) {
+		next(err);
+	}
 };
 
 export const indexFileInfoGet = async (req, res, next) => {
-    try {
-        await renderIndex(req, res, { fileInfoId: req.params.file_id });
-    } catch (err) { next(err); }
+	try {
+		await renderIndex(req, res, { fileInfoId: req.params.file_id });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const indexEditFolderGet = async (req, res, next) => {
+	try {
+        
+		await renderIndex(req, res, { editFolderId: req.params.folder_id });
+	} catch (err) {
+		next(err);
+	}
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as indexController from '../controllers/indexController.js';
-import { indexGet, indexEditFileGet, indexFileInfoGet } from '../controllers/indexController.js';
+import { indexGet, indexEditFileGet, indexFileInfoGet, indexEditFolderGet} from '../controllers/indexController.js';
 
 const indexRouter = Router();
 
@@ -8,5 +8,7 @@ indexRouter.get('/', indexGet)
 
 indexRouter.get('/editFile/:file_id', indexEditFileGet)
 indexRouter.get('/fileInfo/:file_id', indexFileInfoGet)
+
+indexRouter.get('/editFolder/:folder_id', indexEditFolderGet)
 
 export default indexRouter;
