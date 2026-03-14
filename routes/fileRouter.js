f33e441283eee30eba92a0568d7a2c7cfd026121
Jason <jason13.deng@gmail.com>
2026-03-14 14:58:17 +0900

    feat: update index css

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -14,7 +14,6 @@ const fileRouter = Router();
 
 fileRouter.get('/upload', ensureLoggedIn, fileUploadGet);
 fileRouter.post('/upload', ensureLoggedIn, fileUploadPost);
-
 fileRouter.post('/delete/:file_id', fileDeletePost);
 fileRouter.post('/edit/:file_id', fileEditPost);
 fileRouter.get('/info/:file_id', fileInfoGet);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,15 +6,12 @@ import { getAllFolders } from '../controllers/folderController.js';
 const indexRouter = Router();
 
 indexRouter.get('/', async (req, res, next) => {
-    
     if (req.user) {
         const folders = await getAllFolders(req.user.id)
         res.render('index', {folders, styles: 'index.css'})
     }　else {
         res.render('index', {styles: 'index.css'})
     }
-    
-    
 })
 
 export default indexRouter;
